3a86c9597685eaa17aa34af6ae660a6e0c8cbe1a
hitomi.m <1h1t0m3.f@gmail.com>
2020-03-01 12:09:40 +0900

    WIP: card component add README

---
+++ README.md
@@ -0,0 +1,3 @@
+ionic build
+ionic capacitor add android
+npx cap open android
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Redirect, Route } from 'react-router-dom';
-import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
+import { IonCard, IonCardContent, IonIcon } from '@ionic/react';
+import { heartOutline } from 'ionicons/icons';
 import './Card.scss';
 
 interface CardProps {
@@ -15,8 +16,22 @@ const Card: React.FC<CardProps> = ({}) => {
       </IonCardHeader> */}
 
       <IonCardContent mode="md" className="card-wrap">
-        <div className="left">aaa</div>
-        <div className="right">aaa</div>
+        <div className="card-content left">
+          <div className="color-box first"></div>
+          <div className="color-box second"></div>
+          <div className="color-box third"></div>
+        </div>
+        <div className="card-content right">
+          <div className="upper">
+            <span className="date">2020/02/29</span>
+            <IonIcon icon={ heartOutline } className='icon favorite' />
+          </div>
+          <div className="lower">
+            <span className="tag">青</span>
+            <span className="tag">赤</span>
+            <span className="tag">黄</span>
+          </div>
+        </div>
       </IonCardContent>
     </IonCard>
   );
