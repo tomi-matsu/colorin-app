7600034446ea9dc7fd7acc88e666e2d7be83f36f
hitomi.m <1h1t0m3.f@gmail.com>
2020-03-06 16:19:49 +0900

    add firebase get firestore data

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import firebase, { db } from '../firebase';
 import Card from '../components/Card';
 
 import './HomeContainer.scss';
+
+const docRef = db.collection("items").doc("iAHl8KwVQt7SLHiuMgqv");
+console.log(docRef);
+docRef.get().then(function(doc) {
+  if (doc.exists) {
+      console.log("Document data:", doc.data());
+  } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+  }
+}).catch(function(error) {
+  console.log("Error getting document:", error);
+});
 
 interface ContainerProps {
   name: string;
---
+++ src/firebase.tsx
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+// import firestore from 'firebase/firestore'
+
+// const settings = {timestampsInSnapshots: true};
+const firebaseConfig: object = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STRAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
+// firebase.firestore().settings(settings);
+
+export default firebase;
+export const db = firebase.firestore();
