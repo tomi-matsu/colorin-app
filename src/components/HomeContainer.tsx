398e8953768fcdfc1d30386aca782a288bfabc08
hitomi.m <1h1t0m3.f@gmail.com>
2020-03-09 17:35:42 +0900

    add redux

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import firebase, { db } from '../firebase';
 import Card from '../components/Card';
 
 import './HomeContainer.scss';
-
-const docRef = db.collection("items").doc("iAHl8KwVQt7SLHiuMgqv");
-console.log(docRef);
-docRef.get().then(function(doc) {
-  if (doc.exists) {
-      console.log("Document data:", doc.data());
-  } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-  }
-}).catch(function(error) {
-  console.log("Error getting document:", error);
-});
 
 interface ContainerProps {
   name: string;
@@ -23,7 +9,7 @@ interface ContainerProps {
 
 const HomeContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
+    <div className="home-container">
       <Card />
       <Card />
       <Card />
