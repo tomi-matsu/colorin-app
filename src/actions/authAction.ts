097c32952389eaf8c604201d3491dafa17b54223
hitomi.m <1h1t0m3.f@gmail.com>
2020-06-23 16:16:25 +0900

    add capacitor-firebase-auth

--- src/actions/authAction.ts
+++ src/actions/authAction.ts
@@ -1,6 +1,8 @@
 import firebase from '../firebase'
-import '@codetrix-studio/capacitor-google-auth';
-import { Plugins } from '@capacitor/core';
+// import '@codetrix-studio/capacitor-google-auth';
+// import { Plugins } from '@capacitor/core';
+import { User } from 'firebase/app'
+import { cfaSignIn } from 'capacitor-firebase-auth';
 
 // リクエスト開始時
 export const AUTH_REQUEST = 'AUTH_REQUEST'
@@ -33,24 +35,29 @@ export const googleLogin = (): Function => {
   console.log('%c==================actions/authActions: googleLogin', 'color: red')
   return (dispatch: any) => {
     dispatch(authRequest())
-    const googleUser: any = {}
-    Plugins.GoogleAuth.signIn()
-      .then(async (googleUser: { authentication: { idToken: any; }; name: any; }) => {
-        googleUser = googleUser
-        console.log('%c==================actions/authActions: auth success', 'color: red')
-        console.log(googleUser)
-        // firebase.auth().onAuthStateChanged(function(user) {
-        //   if (user) {
-        //     console.log(user)
-        //     // User is signed in.
-        //   } else {
-        //     // No user is signed in.
-        //   }
-        // });
-        dispatch(authSuccess(googleUser))
-      }).catch((error: { code: any; }) => {
-        console.log('%c==================actions/authActions: auth error', 'color: red')
-        dispatch(authFailure(error))
-      })
+    // const googleUser: any = {}
+    cfaSignIn('google.com').subscribe(
+      (user: User) => console.log(user.displayName)
+    )
+    // Plugins.GoogleAuth.signIn()
+    //   .then(async (googleUser: { authentication: { idToken: any; }; name: any; }) => {
+    //     googleUser = googleUser
+    //     console.log('%c==================actions/authActions: auth success', 'color: red')
+    //     console.log(googleUser)
+    //     const credential = Plugins.GoogleAuthProvider.credential(googleUser.authentication.idToken);
+    //     console.log(credential)
+    //     // firebase.auth().onAuthStateChanged(function(user) {
+    //     //   if (user) {
+    //     //     console.log(user)
+    //     //     // User is signed in.
+    //     //   } else {
+    //     //     // No user is signed in.
+    //     //   }
+    //     // });
+    //     dispatch(authSuccess(googleUser))
+    //   }).catch((error: { code: any; }) => {
+    //     console.log('%c==================actions/authActions: auth error', 'color: red')
+    //     dispatch(authFailure(error))
+    //   })
   }
 }
